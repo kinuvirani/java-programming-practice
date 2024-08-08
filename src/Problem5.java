f3d05136ba578b3ed9aefcfaaf9b0ce77e5cd28c
kiran-lns <kiran@lockandstock.app>
2024-08-08 11:05:43 -0400

    Print 50 even numbers

---
+++ src/Problem5.java
@@ -0,0 +1,14 @@
+
+public class Problem5 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 50) {
+            if (i % 2 == 0) {
+                System.out.println("Even Number is " + i);
+            }
+            i += 1;
+        }
+    }
+}
+
+//Note: if I want to use array, how can I push numbers? int [] arr = {};

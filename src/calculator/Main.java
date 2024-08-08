83e6c29f4b76e2bde59d0ae0afd0694544123c8c
kiran-lns <kiran@lockandstock.app>
2024-08-08 15:06:54 -0400

    Fixed calculator

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -3,46 +3,53 @@ package calculator;
 import java.util.Scanner;
 
 public class Main {
+    public static boolean divideByZero(double num) {
+        return num == 0;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
-        String operation;
+        String operation = "";
         double result = 0.0;
         do {
             System.out.println("Enter a calculator operation you want to perform in this format(10+20): ");
-            operation = scanner.next();
-            if (!operation.matches("^\\d+[+\\-*/]\\d+$")) {
-                System.out.println("Please enter a valid input.");
-                return;
-            }
-            String[] numbers = operation.split("[+-/*]");
-            char operator = operation.charAt(numbers[0].length());
-            double number1 = Double.parseDouble(numbers[0]);
-            double number2 = Double.parseDouble(numbers[1]);
+            operation = scanner.nextLine();
 
-            switch (operator) {
-                case '+':
-                    Addition addition = new Addition(number1, number2);
-                    result = addition.getAddition();
-                    break;
-                case '-':
-                    Subtraction subtraction = new Subtraction(number1, number2);
-                    result = subtraction.getSubtraction();
-                    break;
-                case '*':
-                    Multiplication multiplication = new Multiplication(number1, number2);
-                    result = multiplication.getMultiplication();
-                    break;
-                case '/':
-                    Division division = new Division(number1, number2);
-                    result = division.getDivision();
-                    break;
-                default:
-                    System.out.println("Invalid Operation");
-                    return;
+            if (operation.matches("\\d+[+\\-*/]\\d+$")) {
+                String[] numbers = operation.split("[+-/*]");
+                char operator = operation.charAt(numbers[0].length());
+                double number1 = Double.parseDouble(numbers[0]);
+                double number2 = Double.parseDouble(numbers[1]);
+
+                switch (operator) {
+                    case '+':
+                        Addition addition = new Addition(number1, number2);
+                        result = addition.getAddition();
+                        break;
+                    case '-':
+                        Subtraction subtraction = new Subtraction(number1, number2);
+                        result = subtraction.getSubtraction();
+                        break;
+                    case '*':
+                        Multiplication multiplication = new Multiplication(number1, number2);
+                        result = multiplication.getMultiplication();
+                        break;
+                    case '/':
+                        boolean isDivideByZero = divideByZero(number2);
+                        if (isDivideByZero) {
+                            System.out.println("Divide by zero exception");
+                            break;
+                        }
+                        Division division = new Division(number1, number2);
+                        result = division.getDivision();
+                        break;
+                    default:
+                        System.out.println("Invalid Operation");
+                        return;
+                }
+                System.out.println("Your result is for " + number1 + " " + operation + " " + number2 + " = " + result);
+            }else if (!operation.equalsIgnoreCase("x")) {
+                System.out.println("Please enter a valid input.");
             }
-            System.out.println("Your result is for " + number1 + " " + operation + " " + number2 + " = " + result);
         } while (!operation.equalsIgnoreCase("x"));
     }
 }

c172ee400cd691e3c54198ad3b3fd6c86dc53687
kiran-lns <kiran@lockandstock.app>
2024-08-08 10:54:26 -0400

    First 50 odd number sum

---
+++ src/Problem4.java
@@ -0,0 +1,12 @@
+public class Problem4 {
+    public static void main(String[] args) {
+        int i = 1, sum = 0;
+        while (i <= 50) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+            i += 1;
+        }
+        System.out.println("Sum of first 50 odd numbers is: " + sum);
+    }
+}
